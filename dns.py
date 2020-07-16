66153926cea4aee4fa3ffc599acfbfbeda4a276b
Milan Aleksić <milanaleksic@gmail.com>
2020-07-16 14:36:01 +0200

    don't use JSON, use Alfred to keep settings

--- dns.py
+++ dns.py
@@ -1,41 +1,46 @@
 # encoding: utf-8
 import argparse
-import json
 import subprocess
 import sys
 
-from workflow import Workflow, ICON_WEB
+from workflow import Workflow, ICON_WEB, ICON_WARNING
 
 log = None
 
 
 def main(wf):
-    config = _load_config()
     parser = argparse.ArgumentParser()
+    parser.add_argument('--new-dns', dest='dns', nargs='?', default=None)
     parser.add_argument('query', nargs='?', default=None)
     args = parser.parse_args(wf.args)
     query = args.query
+    if args.dns:
+        wf.settings[args.dns] = query
+        return 0
     if query is not None:
+        dns = wf.settings.get(query, None)
+        if dns is None:
+            wf.add_item('No DNS server set',
+                        'Please use dnsset to add your Pinboard API key.',
+                        valid=False,
+                        icon=ICON_WARNING)
+            wf.send_feedback()
+            return 0
         log.info("Running config '%s'" % query)
-        returned_output = subprocess.check_output(["networksetup", "-setdnsservers", "Wi-Fi", config[query]])
+        returned_output = subprocess.check_output(["networksetup", "-setdnsservers", "Wi-Fi", dns])
         log.debug("output of networksetup: %s" % returned_output)
         log.debug("Cleaning cache")
         returned_output = subprocess.check_output(["dscacheutil", "-flushcache"])
         log.debug("output of flush cache: %s" % returned_output)
     else:
-        for k in config.keys():
+        for k in wf.settings.keys():
             wf.add_item(title="Use %s setup" % k,
-                        subtitle="Server %s will be used" % (config[k]),
+                        subtitle="Server %s will be used" % (wf.settings[k]),
                         arg=k,
                         valid=True,
                         icon=ICON_WEB)
         # Send the results to Alfred as XML
         wf.send_feedback()
-
-
-def _load_config():
-    with open("setup.json", "r") as f:
-        return json.load(f)
 
 
 if __name__ == u"__main__":
